41bc5279e1c7febc60f49ca3fc3271b6387573be
Tyler Machell <44388317+TylerMachell@users.noreply.github.com>
2019-10-24 19:42:04 -0700

    Website without python
    new update

--- My Website/mySite/apps/webApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-from django.conf.urls.static import static
-                    
-urlpatterns = [
-    url(r'^$', views.index),
-]
--- My Website/mySite/apps/webApp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, redirect
-from .models import *
-
-# Create your views here.
-def index(request):
-    return render(request, 'webApp/index.html')
